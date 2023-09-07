cf097dd3819fdbe48f670fc699c77b4d32230746
Abdoul <abdoulramanediallo44@gmail.com>
2023-09-07 13:30:08 +0000

    Testing factorial passed

--- solver.rb
+++ solver.rb
@@ -1,2 +1,13 @@
 class Solver
+  def factorial(n)
+    if n.negative?
+      raise ArgumentError, 'Factorial with negative number is not defined'
+    elsif n.zero?
+      1
+    else
+      result = 1
+      (1..n).each { |i| result *= i }
+      result
+    end
+  end
 end
